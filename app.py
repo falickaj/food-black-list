c9f33666bbe9069257a3ca1725df69044474d10d
Fitim Alickaj <fitimalickaj@Fitims-MacBook-Air.local>
2022-09-12 13:05:51 +0200

    new framework

--- app.py
+++ app.py
@@ -1,112 +1,64 @@
-from email.policy import strict
-from re import T
-import re
-from flask import Flask,request,jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-import os
-import json
-from pathlib import Path
-from flask_cors import CORS,cross_origin
-## APP
-app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
-cors = CORS(app)
-
-ENV = 'production'
-
-basedir = os.path.abspath(os.path.dirname(__file__)) 
-
-if ENV == 'dev':
-	app.debug = True
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
-else:
-	app.debug = False
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init DB and MA
-db =SQLAlchemy(app)
-ma = Marshmallow(app)
-
-@app.before_first_request
-def check_db_data():
-	filedb = "db.sqlite"
-	path = Path(filedb)
-	if path.is_file():
-		pass
-	else:
-		db.create_all()
-# Product Class/Model	
-class Product(db.Model):
-	id = db.Column(db.Integer,primary_key=True)
-	name = db.Column(db.String(100),unique=True)
-	description = db.Column(db.String(255))
-
-	def __init__(self,name,description):
-		self.name = name
-		self.description = description
-
-# Product Schema
-class ProductSchema(ma.Schema):
-	class Meta:
-		fields = ('name','description','id')
-
-# Init Schema
-product_schema = ProductSchema()
-products_schema = ProductSchema(many=True)
-
-# / Just to remove flask white page
-@app.route('/')
-def hello():
-	return "Hello there"
+from fastapi import FastAPI
+from redis_om import HashModel,get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
 
 
-# Create Product
-@app.route('/product',methods=['POST'])
-def add_product():
-	name = request.json['name']
-	description = request.json['description']
-	name = name.lstrip()
-	description = description.lstrip()
-	new_product = Product(name,description)
-	db.session.add(new_product)
-	db.session.commit()
 
-	return product_schema.jsonify(new_product)
-
-# Get ALL Products
-@app.route('/product',methods=['GET'])
-def get_products():
-	all_products = Product.query.all()
-	results = products_schema.dump(all_products)
-	return jsonify(results)
-
-# Get One Product with ID
-@app.route('/product/<id>',methods=['GET'])
-def get_product(id):
-	product = Product.query.get(id)
-	return product_schema.jsonify(product)
+app = FastAPI()
 
 
-# Update Product
-@app.route('/product/<id>',methods=['PUT'])
-def update_product(id):
-	product = Product.query.get(id)
-	name = request.json['name']
-	description = request.json['description']
 
-	product.name = name
-	product.description = description
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
-	db.session.commit()
-	return product_schema.jsonify(product)
+redis = get_redis_connection(
+    host="redis-15599.c293.eu-central-1-1.ec2.cloud.redislabs.com",
+    port=15599,
+    password="6PdZ4Uh60c962MKsIw16LVhU6oCFagjU",
+    decode_responses=True
+)
 
 
-# Delete Product
-@app.route('/product/<id>',methods=['DELETE'])
-def delete_product(id):
-	product = Product.query.get(id)
-	db.session.delete(product)
-	db.session.commit()
-	return  jsonify({"Status":"OK"}) 
+class Food(HashModel):
+    name: str
+    description: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Food",
+                "description": "Food"
+            }
+        }
+
+    class Meta:
+        database = redis
+
+
+
+@app.get('/product')
+async def get_product():
+    return [format(pk) for pk in Food.all_pks()]
+
+def format(pk:str):
+    food = Food.get(pk)
+    return {
+        'id':food.pk,
+        'name':food.name,
+        'description': food.description
+    }
+
+
+@app.post('/product')
+async def add_new_product(food:Food):
+    return food.save()
+
+
+@app.delete('/product/{pk}')
+async def remove_product(pk:str):
+    return Food.delete(pk)
+    
